4f9033c2743120bb51cc1721b5ffa07b490e1721
Jonathan Eddy <jonathanceddy@gmail.com>
2016-07-14 11:56:46 -0400

    going back to herokuapp domain

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,7 +69,7 @@ Rails.application.configure do
     :address        => ENV['MAILGUN_SMTP_SERVER'],
     :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
     :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-    :domain         => ENV['MAILGUN_DOMAIN'],
+    :domain         => 'codegray.herokuapp.com',
     :authentication => :plain,
   }
 

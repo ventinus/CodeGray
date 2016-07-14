f903f2e73e199ec6453276f8207bd276d8e17d6e
Jonathan Eddy <jonathanceddy@gmail.com>
2016-07-14 11:42:58 -0400

    changed prod domain

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,7 +69,7 @@ Rails.application.configure do
     :address        => ENV['MAILGUN_SMTP_SERVER'],
     :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
     :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-    :domain         => 'codegray.herokuapp.com',
+    :domain         => ENV['MAILGUN_DOMAIN'],
     :authentication => :plain,
   }
 

e8305beafb985ef465c7a57a59fb1c33c6a135f0
Jonathan Eddy <jonathanceddy@gmail.com>
2016-07-14 23:11:49 -0400

    added form validation in contact form

--- app/assets/javascripts/modules/inquiry-email.js
+++ app/assets/javascripts/modules/inquiry-email.js
@@ -5,17 +5,93 @@ window.MODULES.inquiryEmail = function() {
   };
 
   var els = {};
+  var fields = {};
+
+  var emailField = function(field, value) {
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (!re.test(value)) {
+      createError(field, 'must be an email address.');
+      return false;
+    }
+
+    return true;
+  }
+
+  var requiredField = function(field, value) {
+    if (value == "" || value == null) {
+      createError(field, "field can't be blank.");
+      return false;
+    }
+
+    return true;
+  }
+
+  var fieldValidationMapping = {
+    'email': emailField,
+    'name': requiredField,
+    'message': requiredField
+  }
 
   var createChildren = function() {
     els.form = document.querySelector('.js-contact-form');
+    els.submitBtn = els.form.querySelector('input[type="submit"]');
     els.successMessage = document.querySelector('.js-contact-form-success');
+
+    fields.email = els.form.querySelector('#inquiry_email');
+    fields.name = els.form.querySelector('#inquiry_name');
+    fields.message = els.form.querySelector('#inquiry_message');
     return;
   }
 
   var onSubmit = function(e) {
     e.preventDefault();
+    els.submitBtn.disabled = true;
 
-    var data = new FormData(els.form);
+    removePreviousErrors();
+
+    var isValid = validateForm();
+
+    if (!isValid) {
+      els.submitBtn.disabled = false;
+      return;
+    }
+
+    sendData();
+    return;
+  }
+
+  var createError = function(field, message) {
+    var errorEl = document.createElement('p');
+    errorEl.classList.add('form-error');
+    errorEl.innerText = message;
+    fields[field].parentElement.appendChild(errorEl);
+
+    return;
+  }
+
+  var validateForm = function() {
+    var isValid = true;
+    for (field in fields) {
+      var value = fieldValidationMapping[field](field, fields[field].value);
+      if (!value) {
+        isValid = value;
+      }
+    }
+
+    return isValid;
+  }
+
+  var removePreviousErrors = function() {
+    var errors = els.form.querySelectorAll('.form-error');
+
+    for (var i = errors.length - 1; i >= 0; i--) {
+      errors[i].parentElement.removeChild(errors[i]);
+    }
+
+    return;
+  }
+
+  var sendData = function() {
     var xmlhttp = new XMLHttpRequest();
     xmlhttp.open('POST', '/inquiries', true);
 
@@ -32,7 +108,9 @@ window.MODULES.inquiryEmail = function() {
       }
     }
 
+    var data = new FormData(els.form);
     xmlhttp.send(data);
+    return;
   }
 
   var onSuccess = function(e) {
